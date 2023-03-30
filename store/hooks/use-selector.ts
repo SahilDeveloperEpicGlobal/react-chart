a919fd095307f7b00c812d8c948227f20d6a10d0
simpleneeraj <simplneeraj@gmail.com>
2023-03-30 16:10:43 +0530

    in js

--- store/hooks/use-selector.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector } from "react-redux";
-import { RootState } from "@/typings/store";
-import type { TypedUseSelectorHook } from "react-redux";
-
-const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export default useAppSelector;
